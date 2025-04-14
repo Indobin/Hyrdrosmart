332d2f3aed7a9a53420d436111e02b96e319b897
Satya788. <fegotero7@gmail.com>
2025-04-14 17:36:43 +0700

    add seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'username' => 'Sengon',
+            'username' => 'Satya',
             'email' => 'satyabr147@gmail.com',
         ]);
     }
